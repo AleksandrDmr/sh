9c4fcc2b902ae17a1b23fac85ebcd01ac76df134
Aleksandr <alleksdm1995@gmail.com>
2021-03-02 01:29:51 +0500

    init yii framework

---
+++ config/web.php
@@ -0,0 +1,6 @@
+<?
+
+return [
+  'id' => 'school-web',
+  'basePath' => realpath(__DIR__ . '/../')
+];
---
+++ controllers/SiteController.php
@@ -0,0 +1,11 @@
+<?
+namespace app\controllers;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+   public function actionIndex()
+   {
+        return $this->render('index');
+   }
+}
